b2791fa0eed09a9fa612fd58c41c7dc406b950d0
jehovander delgado <110688580+jehovander@users.noreply.github.com>
2023-01-26 02:39:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import nltk
+nltk.download('punkt')
 from flask import Flask , render_template , request , jsonify
 
 from chat import get_response
- import nltk
- nltk.download('punkt')
+
 app = Flask ( __name__ )
 
 
